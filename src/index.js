a569e1f0607074c73549324170d2c2041b9399e6
MichelleMapleston <95059671+MichelleMapleston@users.noreply.github.com>
2022-01-23 12:09:43 +1100

    Added humidity, wind & feels like live data

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ function displayCurrentData(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  //let humidityElement = document.querySelector("#humidity");
-  //let windElement = document.querySelector("#wind");
-  //let precipitationElement = document.querySelector("#precipitation");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let feelsElement = document.querySelector("#feels");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
-  //humidityElement.innerHTML =
-  //windElement.innerHTML
-  //precipitationElement.innerHTML
-  console.log(response.data);
+  humidityElement.innerHTML = `${response.data.main.humidity}%`;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+  feelsElement.innerHTML = `${Math.round(response.data.main.feels_like)}°C`;
 }
 
 let apiKey = "d9d69bcfd71a3f130af2081484a0b61a";
