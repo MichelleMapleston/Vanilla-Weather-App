7b4e233d2757bc930d84b609516213b87c6615af
MichelleMapleston <95059671+MichelleMapleston@users.noreply.github.com>
2022-01-23 13:03:34 +1100

    Updated weather icon with live city data

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ function displayCurrentData(response) {
   let windElement = document.querySelector("#wind");
   let feelsElement = document.querySelector("#feels");
   let dateElement = document.querySelector("#date-time");
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].main;
@@ -30,10 +31,14 @@ function displayCurrentData(response) {
   windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
   feelsElement.innerHTML = `${Math.round(response.data.main.feels_like)}°C`;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  console.log(response.data);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let city = "Sydney";
+let city = "Adelaide";
 let apiKey = "d9d69bcfd71a3f130af2081484a0b61a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
